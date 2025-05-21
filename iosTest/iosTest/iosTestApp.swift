1a5f3a2f0f780cafe0048a6821a631caddf8c290
Argentino Ducret <tino@Argentinos-MacBook-Pro.local>
2025-05-21 16:03:29 -0300

    iOSTest

---
+++ iosTest/iosTest/iosTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosTestApp.swift
+//  iosTest
+//
+//  Created by Argentino Ducret on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
